a5af925fac2b64fe4a6ed1792b46aaa476204a91
ahadalichowdhury <smahadalichowdhury@gmail.com>
2023-09-05 20:40:43 +0600

    second commit

--- src/App.js
+++ src/App.js
@@ -3,13 +3,17 @@ import { useState } from "react";
 
 function App() {
   const [count, setCount] = useState(0);
-  const countTheApp = () => {
+  const countTheAppIncrement = () => {
     setCount(count + 1);
+  }
+  const countTheAppDecrement = () => {
+    setCount(count - 1);
   }
   return (
     <div data-test='component-app'>
-      <h1>The counter value is: {count}</h1>
-     <button onClick={countTheApp}>Click Here</button>
+      <h1 data-test="display-component">The counter value is: <p data-test="count">{count}</p></h1>
+     <button onClick={countTheAppIncrement} data-test="increment-button">Click Here to increment</button>
+     <button onClick={countTheAppDecrement} data-test="decrement-button">Click Here to decrement</button>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -5,26 +5,76 @@ import EnzymeAdapter from '@wojtekmaj/enzyme-adapter-react-17';
 
 Enzyme.configure({adapter:new EnzymeAdapter()});
 
+//ekhon amra bar bar jate repeat na korte hoy ei jonno common korar jonno ekta function banabo
+//jate amra bar bar shallow wrapper create na korte hoy
+
+/** 
+ * Factory function to create a ShallowWrapper for the App component
+ * @function setup
+ * @returns {ShallowWrapper}
+ */
+
+const setup = () => shallow(<App/>)
+
+const findByTestAttributes = (wrapper, val) => wrapper.find(`[data-test='${val}']`)
+
+
+// test("render without errors",()=>{
+//   const wrapper = shallow(<App/>)
+//   const appComponent = wrapper.find("[data-test='component-app']")
+//   expect(appComponent.length).toBe(1)
+  
+// })
 
 test("render without errors",()=>{
-  const wrapper = shallow(<App/>)
-  const appComponent = wrapper.find("[data-test='component-app']")
+  const wrapper = setup()
+  const appComponent = findByTestAttributes (wrapper, "component-app")
   expect(appComponent.length).toBe(1)
   
 })
 
 test("render increment button",()=>{
-  
+  const wrapper = setup()
+  const button = findByTestAttributes(wrapper, "increment-button")
+  expect(button.length).toBe(1)
 })
 
 test("render counter display",()=>{
-  
+  const wrapper = setup()
+  const displayComponent = findByTestAttributes(wrapper, "display-component")
+  expect(displayComponent.length).toBe(1)
 })
 
+//find the .text() method in the enzyme documentation. This always return a string
 test("render display start at 0",()=>{
-  
+  const wrapper = setup()
+  const count = findByTestAttributes(wrapper, "count").text()
+  expect(count).toBe("0")
 })
 
 test("clicking on button increments counter display",()=>{
-  
+  //find the button
+  const wrapper = setup()
+  const button = findByTestAttributes(wrapper, "increment-button")
+
+  //click the button
+  //for clicking the button, we need to use the .simulate() method
+  button.simulate("click")
+  //find the display , and test that the number has been incremented
+  const count = findByTestAttributes(wrapper, "count").text()
+  expect(count).toBe("1")
+
+})
+
+test("clicking on button to decrement the value", ()=>{
+  //find the decrement button
+  const wrapper = setup()
+  const decrementButton = findByTestAttributes(wrapper, "decrement-button")
+
+  //click the decrement button
+  decrementButton.simulate("click")
+
+  //find the display and test that the number has been decremented
+  const count = findByTestAttributes(wrapper, "count").text()
+  expect(count).toBe("-1")
 })
